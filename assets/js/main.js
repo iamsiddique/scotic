88b0344a6361b2c25d4d8d33c99df3f193f5a198
itsmesiddique22 <itsmesiddique22@gmail.com>
2017-12-06 23:19:35 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,24 @@ jQuery(document).ready(function ($) {
             enabled: true
         }
     });
-
+/*----------------------------------------------------*/
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
     /*---------------------------------------------*
      * Menu Section
      ---------------------------------------------*/
